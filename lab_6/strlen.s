d7fc0d6fa1e879e88866bed02b3ff9f22dee84af
Aline Hesley <alinehesley281@gmail.com>
2023-04-19 10:41:38 -0300

    strlen

--- lab_6/strlen.s
+++ lab_6/strlen.s
@@ -1,22 +1,30 @@
 main:
+    addi sp, sp, -28
+    sw ra, sp, 0 #escrevendo o ra na pilha
+    sw a0, sp, 4
 
-
-    addi  t0, zero, 6 #le uma string do teclado
+    addi  t0, zero, 6 #le uma string do teclado e guarda em a0
     ecall
+
+    call strlen
+
+    addi t0, zero, 1 #imprime
+    ecall
+
+    lw ra, sp, 0 #carrega o valor de ra q estava antes da main
+    addi sp, sp, 4
+    ret
 
 strlen:
     addi t0, zero, 0 #contador
 
     strlen_loop:
         lbu t1, a0, 0 #t1 eh vetor *s
-        beq t1, #igual a espaco, str_end
-        addi t0, t0, 1
-        addi a0, a0, 4 #andando no vetor
+        beq t1, zero, str_end
+        addi t0, t0, 1 #somando no cont
+        addi a0, a0, 1 #andando no vetor
         j strlen_loop
 
 str_end:
+    add a0, zero, t0 #movendo o cont p a0
     ret
-
-
-#li a0, 1
-#add t0, zero, 1
